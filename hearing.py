121fa465b9ed1c9145dc070f8f2b50cfd549b166
Đỗ Ngọc Thiên Bảo <ngocthienbaod@gmail.com>
2026-02-09 18:14:51 +0700

    v0.01dev
    blablabla subscribe

---
+++ hearing.py
@@ -0,0 +1,45 @@
+import sounddevice as sd
+import soundfile as sf
+import numpy as np
+from faster_whisper import WhisperModel
+
+import sys
+import os
+
+from termcolor import colored, cprint
+
+print(colored("STARTING EARS!!!", "yellow", attrs=["bold"]))
+
+# ! CONFIGURATION
+model_size = "small" # Model size can be "tiny", "base", "small", "medium", "large"
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+sd.default.device = (1, None)
+print(f"Using device: {sd.query_devices(sd.default.device[0])['name']}")
+
+def record_audio(filename="input.wav", duration=5, samplerate=16000):
+    print(colored("Listening...", "green", attrs=["bold", "blink"]))
+    
+    audio_data = sd.rec(int(duration*samplerate), samplerate=samplerate, channels=1, dtype='float32')
+    
+    sd.wait()
+    
+    sf.write(filename, audio_data, samplerate)
+    print(colored("Done listening :D", "green", attrs=["bold", "blink"]))
+    
+def transcribe_audio(filename="input.wav"):
+    segments, info =  model.transcribe(filename, beam_size=5)
+    
+    full_text = ""
+    for segment in segments:
+        full_text += segment.text
+    return full_text
+
+
+if __name__ == "__main__":
+    record_audio(duration=5)
+    result = transcribe_audio()
+    print(colored("Transcription Result:", "blue", attrs=["bold"]))
+    print("'", end="")
+    print(result, end="")
+    print("'", end="")
